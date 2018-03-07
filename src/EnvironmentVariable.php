a42ad3b3145380e1d364c6e3544381e05907d93a
Baptiste Langlade <baptouuuu@gmail.com>
2018-03-07 07:59:18 +0100

    add environment variable model

---
+++ src/EnvironmentVariable.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Tower;
+
+use Innmind\Tower\Exception\DomainException;
+use Innmind\Immutable\Str;
+
+final class EnvironmentVariable
+{
+    private const PATTERN = '~^(?<name>[A-Z_]+)=(?<value>.*)$~';
+
+    private $name;
+    private $value;
+
+    public function __construct(string $value)
+    {
+        $value = Str::of($value);
+
+        if (!$value->matches(self::PATTERN)) {
+            throw new DomainException((string) $value);
+        }
+
+        $parts = $value->capture(self::PATTERN);
+
+        $this->name = (string) $parts->get('name');
+        $this->value = (string) $parts->get('value');
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function value(): string
+    {
+        return $this->value;
+    }
+}
